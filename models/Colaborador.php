b2c0a5711a4ba27eb11a5dc73e0f769703d4c853
Daniel Magalhães <magalhaesdaniel2009@gmail.com>
2019-06-04 21:24:08 -0300

    Criação do model para os colaboradores

---
+++ models/Colaborador.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "colaborador".
+ *
+ * @property int $id
+ * @property string $nome
+ * @property string $local_foto
+ * @property string $nome_foto
+ * @property string $data_foto
+ * @property string $foto
+ * @property bool $termos
+ */
+class Colaborador extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'colaborador';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['nome', 'local_foto', 'nome_foto', 'data_foto', 'foto'], 'required'],
+            [['data_foto'], 'safe'],
+            [['termos'], 'boolean'],
+            [['nome', 'local_foto', 'nome_foto', 'foto'], 'string', 'max' => 50],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'nome' => 'Nome',
+            'local_foto' => 'Local Foto',
+            'nome_foto' => 'Nome Foto',
+            'data_foto' => 'Data Foto',
+            'foto' => 'Foto',
+            'termos' => 'Termos',
+        ];
+    }
+}
