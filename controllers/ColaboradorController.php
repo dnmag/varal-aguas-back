e55f2f71ee7fe5b3b428f7f8ea3f5fdf76263ec2
Daniel Magalhães <magalhaesdaniel2009@gmail.com>
2019-06-04 21:49:41 -0300

    Criação do controller rest dos colaboradores

---
+++ controllers/ColaboradorController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace app\controllers;
+
+use yii\rest\ActiveController;
+
+class ColaboradorController extends ActiveController
+{
+    public $modelClass = 'app\models\Colaborador';
+}
